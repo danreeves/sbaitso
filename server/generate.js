508ac090eaab75c896adfb8e03dc928fe138461d
Dan Reeves <heydanreeves@gmail.com>
2017-07-22 23:24:09 +0100

    uhh

--- server/generate.js
+++ server/generate.js
@@ -3,5 +3,6 @@ const execa = require('execa');
 module.exports = async function(text) {
     return execa('./record.sh', [`"${text}"`], {
         shell: '/bin/bash',
+        maxBuffer: 100000000,
     });
 };
