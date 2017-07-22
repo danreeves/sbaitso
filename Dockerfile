058af8bdd8ffdf60d05cea464e2e40a5e62ecc14
Dan Reeves <heydanreeves@gmail.com>
2017-07-22 14:09:42 +0100

    now creates a video uploadable to twitter

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM ubuntu
-RUN apt-get update -y
-RUN apt-get install dosbox alsa-utils pulseaudio socat ffmpeg -y
+RUN apt-get -y update
+RUN apt-get -y install dosbox alsa-utils pulseaudio socat nodejs npm
+RUN apt-get -y install wget
+RUN apt-get install -y autoconf automake build-essential mercurial git libarchive-dev fontconfig checkinstall
+RUN apt-get install -y libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libgnutls-dev
+RUN apt-get install -y libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo libtool libva-dev
+RUN apt-get install -y libbs2b-dev libcaca-dev libopenjpeg-dev librtmp-dev libvpx-dev libvdpau-dev
+RUN apt-get install -y libwavpack-dev libxvidcore-dev lzma-dev liblzma-dev zlib1g-dev cmake-curses-gui
+RUN apt-get install -y libx11-dev libxfixes-dev libmp3lame-dev libx264-dev #libx264-146 libx264-dev
+RUN apt-get install -y libfdk-aac-dev ffmpeg
--- record.sh
+++ record.sh
@@ -9,7 +9,7 @@ pacmd set-default-sink v1
 pacmd set-default-source v1.monitor
 
 # Start the ffmpeg capture to an audio file
-ffmpeg -y -f pulse -i default out.mp3 & FFPID=$!
+ffmpeg -y -f pulse -i default out/out.mp3 & FFPID=$!
 
 # Wait for ffmpeg recording to catch up
 sleep 1
@@ -20,10 +20,32 @@ dosbox -c 'mount C sbaitso' -c 'C:' -c 'SAY.BAT "Hello, This Is Doctor Sbaitso"'
 # kill the ffmpeg recording
 kill $FFPID
 
-# trim the silence
-ffmpeg -y -i out.mp3 -af silenceremove=1:0:-50dB trimmed.mp3
+# trim the silence at start
+ffmpeg -y -i out/out.mp3 -af silenceremove=1:0:-50dB out/trimmed1.mp3
+
+# reverse the audio
+ffmpeg -y -i out/trimmed1.mp3 -af areverse out/reversed.mp3
+
+# trim the silence at end
+ffmpeg -y -i out/reversed.mp3 -af silenceremove=1:0:-50dB out/trimmed2.mp3
+
+# reverse the audio again
+ffmpeg -y -i out/trimmed2.mp3 -af areverse out/alltrimmed.mp3
+
 
 # make a video
-ffmpeg -loop 1 -i sbaitso.png -i trimmed.mp3 -c:a copy -c:v libx264 -shortest out.mp4
+ffmpeg \
+    -y \
+    -loop 1 \
+    -i sbaitso.png \
+    -i out/alltrimmed.mp3 \
+    -c:a aac \
+    -strict -2 \
+    -b:a 64k \
+    -c:v libx264 \
+    -b:v 768K \
+    -shortest \
+    -pix_fmt yuv420p \
+    out/video.mp4
 
 # TODO: https://dev.twitter.com/rest/media/uploading-media#videorecs
