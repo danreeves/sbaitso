dbb2b845a41bbcdebac1f1125a202e7a0bbc19e3
Dan Reeves <heydanreeves@gmail.com>
2017-07-22 21:47:00 +0100

    uhh

--- server/generate.js
+++ server/generate.js
@@ -2,7 +2,6 @@ const execa = require('execa');
 
 module.exports = async function(text) {
     return execa('./record.sh', [`"${text}"`], {
-        detached: true,
         stdio: 'ignore',
         shell: '/bin/bash',
     });
